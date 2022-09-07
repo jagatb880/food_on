84c5c5020def6a87a5a8e71750970cc3c953acb9
jagatb880 <jagatb880@gmail.com>
2022-09-07 09:37:09 +0530

    added reegisteration screen

--- src/app/pages/splash-login/splash-login.page.ts
+++ src/app/pages/splash-login/splash-login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-splash-login',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SplashLoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  goToLogin() {
+    this.router.navigate(['/login'])
+  }
+
+  goToRegister() {
+    this.router.navigate(['/register'])
   }
 
 }
