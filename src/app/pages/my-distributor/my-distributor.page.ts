686dc45e032609f0463d0e98958370e40cf9da17
debiprasad55100 <debiprasad55100@gmail.com>
2022-09-15 01:37:11 +0530

    stepper

--- src/app/pages/my-distributor/my-distributor.page.ts
+++ src/app/pages/my-distributor/my-distributor.page.ts
@@ -6,8 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-distributor.page.scss'],
 })
 export class MyDistributorPage implements OnInit {
+  distrubutionarray: { name: string; invitation: string; color: string; }[];
 
-  constructor() { }
+  constructor() { 
+    this.distrubutionarray =[{'name':'El Agrario','invitation':'SUBMITTED','color':'#f28a5f'},
+    {'name':'International Distributor','invitation':'accepted','color':'#35d097'},
+    {'name':'Cosmic INC','invitation':'RECEIVED','color':'#ffd445'}]
+  }
 
   ngOnInit() {
   }
